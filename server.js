0e6f9b0af3d2dd7e7f5a12cdd1df0ad2b17a0bd9
Shaun Forster <shaun.s.forster@gmail.com>
2021-08-03 11:03:43 +0100

    Updating database

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ const image = require("./controllers/image");
 const db = knex({
   client: 'pg',
   connection: {
-    host: '127.0.0.1',
+    host: 'postgresql-deep-67218',
     user: 'postgres',
-    password: 'Mjolnir',
+    password: '',
     database: 'smart-brain'
   }
 });
